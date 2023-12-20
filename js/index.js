f6cbd0e852c5884fc637021b260e9c9fd5fb078f
min5t <mint0906@gmail.com>
2023-12-21 01:27:19 +0800

    12/21改

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var textWrapper = document.querySelector('.text');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.text .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  })
+  .add({
+    targets: '.text',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
