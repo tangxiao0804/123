d1e0eb052494d122608740c33c2886dd747b9374
TangXiao <146960088+tangxiao0804@users.noreply.github.com>
2023-12-30 03:31:35 +0800

    商品頁跳轉

--- js/product.js
+++ js/product.js
@@ -36,15 +36,15 @@ document.addEventListener("DOMContentLoaded", function () {
             }
 
             if (linkText === "新品") {
-                targetPageNumber = 4;
+                targetPageNumber = 1;
             }
 
             if (linkText === "花束") {
-                targetPageNumber = 5;
+                targetPageNumber = 2;
             }
 
             if (linkText === "玻璃罩") {
-                targetPageNumber = 6;
+                targetPageNumber = 3;
             }
             showPage(targetPageNumber);
         });
@@ -60,6 +60,10 @@ $(document).ready(function () {
 });
 
 function redirectToOtherPage(num) {
+    if (num == 1) {
+        window.location.assign("product.html");
+    }
+
     if (num == 2) {
         window.location.assign("product2.html");
     }
