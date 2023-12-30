196ac93f3f8fca82aa8100070dc6f528f8461ab8
min5t <mint0906@gmail.com>
2023-12-31 06:59:39 +0800

    1231熬一個超想吐

---
+++ js/member.js
@@ -0,0 +1,21 @@
+let currentPage = 1;
+showPage(currentPage);
+
+function showPage(pageNumber) {
+    document.getElementById("page1").style.display = "none";
+    document.getElementById("page2").style.display = "none";
+    document.getElementById("page3").style.display = "none";
+    document.getElementById("page" + pageNumber).style.display = "block";
+}
+
+let currentTag = 1;
+showTag(currentTag);
+
+function showTag(TagNumber) {
+    document.getElementById("tag1").style.display = "none";
+    document.getElementById("tag2").style.display = "none";
+    document.getElementById("tag3").style.display = "none";
+    document.getElementById("tag4").style.display = "none";
+
+    document.getElementById("tag" + TagNumber).style.display = "block";
+}
