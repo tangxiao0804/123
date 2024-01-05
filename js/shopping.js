dafab96ed697ef41f1c1222311ea1bcf28968134
TangXiao <146960088+tangxiao0804@users.noreply.github.com>
2024-01-05 22:58:42 +0800

    半

--- js/shopping.js
+++ js/shopping.js
@@ -5,17 +5,16 @@ function minus(i) {
     document.getElementsByClassName("item_num")[i].value = number;
   } else window.alert("商品數量不得少於1");
 }
+
 function add(i) {
   var number = Number(document.getElementsByClassName("item_num")[i].value);
   if (number < 100) {
     number += 1;
     document.getElementsByClassName("item_num")[i].value = number;
   } else window.alert("商品數量不得超過100");
-
 }
 
 function backToMember() {
   window.alert("已完成結帳\n(按下確認後將跳回會員頁)");
-
   window.location.href = "member.html";
 }
